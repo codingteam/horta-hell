4c09c68f18d307b89b08a0ede67b78fbdbc604b9
ForNeVeR <neverthness@gmail.com>
2013-01-08 22:41:49 +0700

    RoomActor: message logging.

--- src/org/ru/codingteam/horta/CoreActor.scala
+++ src/org/ru/codingteam/horta/CoreActor.scala
@@ -1,10 +1,38 @@
 package org.ru.codingteam.horta
 
-import akka.actor.{ActorLogging, Actor}
-import messages.JoinRoom
+import akka.actor.{Props, ActorLogging, Actor}
+import messages.{UserMessage, JoinRoom}
+import org.jivesoftware.smack.{PacketListener, XMPPConnection}
+import org.jivesoftware.smackx.muc.MultiUserChat
+import org.jivesoftware.smack.packet.{Message, Packet}
 
 class CoreActor extends Actor with ActorLogging {
+  lazy val connection = {
+    val server = Configuration.server
+    log.info(s"Connecting to $server")
+
+    val connection = new XMPPConnection(server)
+    connection.connect()
+    connection.login(Configuration.login, Configuration.password)
+    log.info("Login succeed")
+
+    connection
+  }
+
   def receive = {
-    case JoinRoom(jid) =>
+    case JoinRoom(jid) => {
+      log.info(s"JoinRoom($jid)")
+      val actor = context.system.actorOf(Props[RoomActor], name = jid)
+      val muc = new MultiUserChat(connection, jid)
+      muc.addMessageListener(new PacketListener {
+        def processPacket(packet: Packet) {
+          log.info(s"Packet received from $jid: $packet")
+          packet match {
+            case message: Message => actor ! UserMessage(message.getFrom, message.getBody)
+          }
+        }
+      })
+      muc.join("horta hell")
+    }
   }
 }
