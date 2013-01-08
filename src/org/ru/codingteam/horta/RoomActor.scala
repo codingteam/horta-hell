741e5f4e9b469bdf39320544c64c32df888cb083
ForNeVeR <neverthness@gmail.com>
2013-01-09 00:13:46 +0700

    Core: added libraries. Platonus library incorporated.

--- src/org/ru/codingteam/horta/RoomActor.scala
+++ src/org/ru/codingteam/horta/RoomActor.scala
@@ -1,10 +1,34 @@
 package org.ru.codingteam.horta
 
-import akka.actor.{Actor, ActorLogging}
-import messages.UserMessage
+import akka.actor.{Props, Actor, ActorLogging}
+import messages.{Initialize, SendMessage, UserMessage}
+import platonus.network
+import org.jivesoftware.smackx.muc.MultiUserChat
 
 class RoomActor extends Actor with ActorLogging {
+  var room: MultiUserChat = null
+  var networks = Map[String, network]()
+
   def receive = {
-    case UserMessage(jid, message) => log.info(s"Message from $jid: $message")
+    case Initialize(muc) => room = muc
+    case UserMessage(jid, message) => {
+      if (message == "$say") {
+        val network = networks.get(jid)
+        if (network.isDefined) {
+          val phrase = network.get.doGenerate()
+          sender ! SendMessage(room, phrase)
+        }
+      } else {
+        val network = if (networks.contains(jid)) {
+          networks.get(jid).get
+        } else {
+          val n = new network()
+          networks = networks.updated(jid, n)
+          n
+        }
+
+        network.addPhrase(message)
+      }
+    }
   }
 }
