48ea12871ffca02b535b2916483fb5587a6be419
ForNeVeR <neverthness@gmail.com>
2013-01-09 22:42:40 +0700

    Project: moved to sbt.

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "horta-hell"
+
+version := "0.1-SNAPSHOT"
+
+mainClass in (Compile, run) := Some("org.ru.codingteam.horta.Application")
+
+scalaVersion := "2.10.0"
+
+resolvers ++= Seq(
+	"clojars" at "http://clojars.org/repo/",
+	"clojure-releases" at "http://build.clojure.org/releases")
+
+libraryDependencies += "platonus" % "platonus" % "0.1.1"
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.1.0"
+
+libraryDependencies += "org.igniterealtime.smack" % "smack" % "3.2.1"
+
+libraryDependencies += "org.igniterealtime.smack" % "smackx" % "3.2.1"
--- src/main/scala/org/ru/codingteam/horta/RoomActor.scala
+++ src/main/scala/org/ru/codingteam/horta/RoomActor.scala
@@ -2,31 +2,20 @@ package org.ru.codingteam.horta
 
 import akka.actor.{Props, Actor, ActorLogging}
 import messages.{Initialize, SendMessage, UserMessage}
-import platonus.network
+import platonus.Network
 import org.jivesoftware.smackx.muc.MultiUserChat
 
 class RoomActor extends Actor with ActorLogging {
   var room: MultiUserChat = null
-  var networks = Map[String, network]()
+  var network = new Network()
 
   def receive = {
     case Initialize(muc) => room = muc
     case UserMessage(jid, message) => {
       if (message == "$say") {
-        val network = networks.get(jid)
-        if (network.isDefined) {
-          val phrase = network.get.doGenerate()
-          sender ! SendMessage(room, phrase)
-        }
+        val phrase = network.doGenerate()
+        sender ! SendMessage(room, phrase)
       } else {
-        val network = if (networks.contains(jid)) {
-          networks.get(jid).get
-        } else {
-          val n = new network()
-          networks = networks.updated(jid, n)
-          n
-        }
-
         network.addPhrase(message)
       }
     }
