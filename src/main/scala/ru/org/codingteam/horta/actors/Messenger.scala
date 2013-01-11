7c8cb40bff7e742e8afa570114c80dd29bb887fc
ForNeVeR <neverthness@gmail.com>
2013-01-11 22:07:30 +0700

    Project: plugin system slightly modified.

--- src/main/scala/ru/org/codingteam/horta/actors/Messenger.scala
+++ src/main/scala/ru/org/codingteam/horta/actors/Messenger.scala
@@ -5,7 +5,11 @@ import org.jivesoftware.smack.{PacketListener, XMPPConnection}
 import org.jivesoftware.smackx.muc.MultiUserChat
 import org.jivesoftware.smack.packet.{Message, Packet}
 import ru.org.codingteam.horta.Configuration
-import ru.org.codingteam.horta.messages.{SendMessage, UserMessage, Initialize, JoinRoom}
+import ru.org.codingteam.horta.messages._
+import ru.org.codingteam.horta.messages.Initialize
+import ru.org.codingteam.horta.messages.UserMessage
+import ru.org.codingteam.horta.messages.JoinRoom
+import ru.org.codingteam.horta.messages.SendMessage
 
 class Messenger extends Actor with ActorLogging {
   lazy val connection = {
@@ -21,9 +25,14 @@ class Messenger extends Actor with ActorLogging {
   }
 
   def receive = {
+    case InitializePlugin(core, plugins) => {
+      val self = context.self
+      Configuration.rooms foreach { case (roomName, jid) => self ! JoinRoom(jid) }
+    }
+
     case JoinRoom(jid) => {
       log.info(s"JoinRoom($jid)")
-      val actor = context.system.actorOf(Props[Room], name = jid)
+      val actor = context.system.actorOf(Props[Room]())
       val muc = new MultiUserChat(connection, jid)
       actor ! Initialize(muc)
 
