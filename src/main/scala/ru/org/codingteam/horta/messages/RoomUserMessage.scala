430470c64c7ad8efd363523e437febb485465eda
ForNeVeR <neverthness@gmail.com>
2013-01-14 22:18:19 +0700

    RoomUser: used ask instead of tell. Closes #6.

--- src/main/scala/ru/org/codingteam/horta/messages/RoomUserMessage.scala
+++ src/main/scala/ru/org/codingteam/horta/messages/RoomUserMessage.scala
@@ -7,5 +7,5 @@ abstract class RoomUserMessage
 case class AddPhrase(phrase: String) extends RoomUserMessage
 case class GeneratePhrase(forNick: String) extends RoomUserMessage
 case class CalculateDiff(forNick: String, nick1: String, nick2: String, roomUser2: ActorRef) extends RoomUserMessage
-case class CalculateDiffRequest(room: ActorRef, forNick: String, nick1: String, nick2: String, network1: Network)
+case class CalculateDiffRequest(forNick: String, nick1: String, nick2: String, network1: Network)
   extends RoomUserMessage
