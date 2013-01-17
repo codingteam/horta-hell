c44d291fe76e013b1d6af2307d7d88ce0b1f2069
ForNeVeR <neverthness@gmail.com>
2013-01-17 20:31:29 +0700

    Messenger: all listener classes decoupled. Closes #26.

---
+++ src/main/scala/ru/org/codingteam/horta/actors/messenger/MessageAutoRepeater.scala
@@ -0,0 +1,28 @@
+package ru.org.codingteam.horta.actors.messenger
+
+import org.jivesoftware.smack.PacketListener
+import org.jivesoftware.smack.packet.{Message, Packet}
+import ru.org.codingteam.horta.messages.SendMessage
+import akka.actor.{ActorRef, Scheduler}
+import scala.concurrent.duration._
+import concurrent.ExecutionContext
+
+class MessageAutoRepeater(
+    val messenger: ActorRef,
+    val scheduler: Scheduler,
+    val jid: String,
+    implicit val executor: ExecutionContext) extends PacketListener {
+  def processPacket(packet: Packet) {
+    packet match {
+      case message: Message => {
+        val error = packet.getError
+        if (error != null && error.getCondition == "resource-constraint") {
+          val body = message.getBody
+          scheduler.scheduleOnce(1 second) {
+            messenger ! SendMessage(jid, "[Re]" + body)
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/main/scala/ru/org/codingteam/horta/actors/messenger/MucParticipantListener.scala
@@ -0,0 +1,16 @@
+package ru.org.codingteam.horta.actors.messenger
+
+import akka.actor.ActorRef
+import org.jivesoftware.smack.PacketListener
+import org.jivesoftware.smack.packet.{Packet, Presence}
+import ru.org.codingteam.horta.messages.UserPresence
+
+class MucParticipantListener(val roomActor: ActorRef) extends PacketListener {
+  def processPacket(packet: Packet) {
+    packet match {
+      case presence: Presence => {
+        roomActor ! UserPresence(presence)
+      }
+    }
+  }
+}
