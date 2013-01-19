ca2aec19780442c35246bd81fb505ca0162d49b3
rexim <reximkut@gmail.com>
2013-01-20 02:59:58 +0700

    Core: New parsers have been integrated into bot's infrastructure.

--- src/main/scala/ru/org/codingteam/horta/actors/core/Core.scala
+++ src/main/scala/ru/org/codingteam/horta/actors/core/Core.scala
@@ -11,6 +11,7 @@ import ru.org.codingteam.horta.actors.messenger.Messenger
 class Core extends Actor with ActorLogging {
   var commands = Map[String, Command]()
   var plugins: Map[String, ActorRef] = null
+  val parsers = List(SlashParsers, DollarParsers)
 
   override def preStart() = {
     val messenger = context.actorOf(Props(new Messenger(self)), "messenger")
@@ -44,60 +45,17 @@ class Core extends Actor with ActorLogging {
     }
   }
 
-  val dollarCommandNameRegex = "^\\$([^\\s]+).*?$".r
-  val slashCommandNameRegex = "^([^\\s]+?)/.*?$".r
-
-  def parseCommand(message: String) = {
-    message match {
-      case dollarCommandNameRegex(command) => {
-        commands.get(command) match {
-          case Some(command) => Some(CommandArguments(command, parseDollarArguments(message)))
-          case None          => None
+  def parseCommand(message: String) : Option[CommandArguments] = {
+    for(p <- parsers) {
+      p.parse(p.command, message) match {
+        case p.Success((name, arguments), _) => commands.get(name) match {
+          case Some(command) => return Some(CommandArguments(command, arguments))
+          case None => 
         }
-      }
-
-      case slashCommandNameRegex(command) => {
-        commands.get(command) match {
-          case Some(command) => Some(CommandArguments(command, parseSlashArguments(message)))
-          case None          => None
-        }
-      }
-
-      case _ => None
-    }
-  }
-
-  def parseDollarArguments(message: String) : Array[String] = {
-    val parser = new RegexParsers {
-      override type Elem = Char
-      def command_name = regexMatch("^(\\$[^\\s]+)".r) ^^ {m => m.group(1)}
-      def regular_arg = regexMatch("([^\\s]+)".r) ^^ {m => m.group(1)}
-      def quoted_arg = regexMatch("\"(.*?(?<!\\\\)(\\\\\\\\)*)\"".r) ^^ {m => m.group(1)}
-      def command = command_name ~ ((quoted_arg | regular_arg) *) ^^ {case name~arguments => arguments}
-
-      /* http://stackoverflow.com/questions/1815716/accessing-scala-parser-regular-expression-match-data */
-      def regexMatch(r: Regex): Parser[Regex.Match] = new Parser[Regex.Match] {
-        def apply(in: Input) = {
-          val source = in.source
-          val offset = in.offset
-          val start = handleWhiteSpace(source, offset)
-          (r findPrefixMatchOf (source.subSequence(start, source.length))) match {
-            case Some(matched) =>
-              Success(matched, in.drop(start + matched.end - offset))
-            case None =>
-              Failure("string matching regex `"+r+"' expected but `"+in.first+"' found", in.drop(start - offset))
-          }
-        }
+        case _ =>
       }
     }
-    
-    parser.parse(parser.command, message) match {
-      case parser.Success(arguments, _) => (arguments map {x => x.replace("\\\\", "\\").replace("\\\"", "\"")}).toArray
-      case _ => Array()
-    }
-  }
 
-  def parseSlashArguments(message: String) = {
-    message.split("/", -1).tail
+    None
   }
 }
