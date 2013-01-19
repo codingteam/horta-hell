1ff4de2899d71a120e5285cf6a8644dcb789eef3
rexim <reximkut@gmail.com>
2013-01-20 03:19:39 +0700

    PetMessage: case classes without a parameter list have been deprecated

--- src/main/scala/ru/org/codingteam/horta/messages/PetMessage.scala
+++ src/main/scala/ru/org/codingteam/horta/messages/PetMessage.scala
@@ -4,4 +4,4 @@ import akka.actor.ActorRef
 
 abstract class PetMessage
 case class PetCommand(command: Array[String]) extends PetMessage
-case class PetTick extends PetMessage
+case class PetTick() extends PetMessage
