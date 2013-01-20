7e9f1c1b36bceb51d7c35650b51864e82f48f8e8
ForNeVeR <neverthness@gmail.com>
2013-01-20 14:52:06 +0700

    Project: small clean up.

--- src/main/scala/ru/org/codingteam/horta/messages/PetMessage.scala
+++ src/main/scala/ru/org/codingteam/horta/messages/PetMessage.scala
@@ -1,7 +1,5 @@
 package ru.org.codingteam.horta.messages
 
-import akka.actor.ActorRef
-
 abstract class PetMessage
 case class PetCommand(command: Array[String]) extends PetMessage
-case class PetTick() extends PetMessage
+case object PetTick extends PetMessage
