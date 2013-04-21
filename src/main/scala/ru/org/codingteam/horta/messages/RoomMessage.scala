06fb25168377dca3a1140234bf917da8850bc772
ForNeVeR <neverthness@gmail.com>
2013-04-21 13:47:25 +0700

    New command execution engine.

--- src/main/scala/ru/org/codingteam/horta/messages/RoomMessage.scala
+++ src/main/scala/ru/org/codingteam/horta/messages/RoomMessage.scala
@@ -4,6 +4,11 @@ import org.jivesoftware.smack.packet.{Message, Presence}
 
 abstract sealed class RoomMessage
 
+/**
+ * Returns room JID as string.
+ */
+case class GetJID() extends RoomMessage
+
 case class UserMessage(message: Message) extends RoomMessage
 
 case class UserPresence(presence: Presence) extends RoomMessage
