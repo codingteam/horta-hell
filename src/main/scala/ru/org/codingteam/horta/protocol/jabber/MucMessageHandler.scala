9a133983c5976859acf177443d44adc395ff7ab0
ForNeVeR <neverthness@gmail.com>
2014-01-21 20:45:05 +0700

    Hopefully fix all user name issues.

--- src/main/scala/ru/org/codingteam/horta/protocol/jabber/MucMessageHandler.scala
+++ src/main/scala/ru/org/codingteam/horta/protocol/jabber/MucMessageHandler.scala
@@ -65,7 +65,7 @@ class MucMessageHandler(val protocol: ActorRef, val roomJid: String) extends Act
     for (nick <- userNicks) {
       if (nick != recipient && nick.length > 0) {
         val quoted = Pattern.quote(nick)
-        val pattern = s"(\\b|\\B)$quoted(\\b|\\B)"
+        val pattern = s"(?<=\\W|^)$quoted(?=\\W|$$)"
         val replacement = nick.substring(0, 1) + "…"
         message = message.replaceAll(pattern, replacement)
       }
