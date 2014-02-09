91ac539e53926b603be89ea0c0e95dc6c54dccbb
ForNeVeR <neverthness@gmail.com>
2014-02-09 22:14:51 +0700

    Fix private message handling.

--- src/main/scala/ru/org/codingteam/horta/protocol/jabber/ChatListener.scala
+++ src/main/scala/ru/org/codingteam/horta/protocol/jabber/ChatListener.scala
@@ -1,6 +1,5 @@
 package ru.org.codingteam.horta.protocol.jabber
 
-import akka.pattern.ask
 import org.jivesoftware.smack.{MessageListener, Chat, ChatManagerListener}
 import akka.actor.ActorRef
 import org.jivesoftware.smack.packet.Message
@@ -10,21 +9,19 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 import concurrent.ExecutionContext
 
-class ChatListener(
-					  val messenger: ActorRef,
-					  val privateHandler: ActorRef,
-					  implicit val executor: ExecutionContext) extends ChatManagerListener {
-	implicit val timeout = Timeout(60 seconds)
+class ChatListener(val messenger: ActorRef,
+                   val privateHandler: ActorRef,
+                   implicit val executor: ExecutionContext) extends ChatManagerListener {
+  implicit val timeout = Timeout(60 seconds)
 
-	def chatCreated(chat: Chat, createdLocally: Boolean) {
-		if (!createdLocally) {
-			for (reply <- messenger.ask(ChatOpened(chat))) {
-				chat.addMessageListener(new MessageListener {
-					def processMessage(chat: Chat, message: Message) {
-						privateHandler ! UserMessage(message)
-					}
-				})
-			}
-		}
-	}
+  def chatCreated(chat: Chat, createdLocally: Boolean) {
+    if (!createdLocally) {
+      messenger ! ChatOpened(chat)
+      chat.addMessageListener(new MessageListener {
+        def processMessage(chat: Chat, message: Message) {
+          privateHandler ! UserMessage(message)
+        }
+      })
+    }
+  }
 }
