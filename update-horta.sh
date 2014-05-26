367fda7170f84cb81b3b95964d17b1eb5d74cc97
ForNeVeR <neverthness@gmail.com>
2014-05-26 22:41:09 +0700

    Configuration: store the configuration and the database on the host machine only. Close #7, #10.

--- update-horta.sh
+++ update-horta.sh
@@ -21,8 +21,6 @@ sbt assembly
 mv target/scala-2.10/horta-hell-assembly.jar ./horta-hell.jar
 sbt clean
 
-cp /vagrant/horta.properties ./horta.properties
-
 # Start the daemon:
 cp /vagrant/horta-hell.conf /etc/init/
 start horta-hell
