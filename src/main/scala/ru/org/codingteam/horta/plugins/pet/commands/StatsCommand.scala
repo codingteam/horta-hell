8de0d8283e3e18cef7489d361d575b443ce57115
Friedrich von Never <fvnever@gmail.com>
2014-08-08 20:14:26 +0700

    Fix age counting. Close #204.

--- src/main/scala/ru/org/codingteam/horta/plugins/pet/commands/StatsCommand.scala
+++ src/main/scala/ru/org/codingteam/horta/plugins/pet/commands/StatsCommand.scala
@@ -13,7 +13,7 @@ class StatsCommand extends AbstractCommand {
         |Кличка: %s
         |Здоровье: %d
         |Голод: %d
-        |Возраст: %d часов""".stripMargin.format(pet.nickname, pet.health, pet.hunger, age.getHours)
+        |Возраст: %d часов""".stripMargin.format(pet.nickname, pet.health, pet.hunger, age.toStandardHours.getHours)
     } else {
       s"%s мертв. Какие еще статы?".format(pet.nickname)
     }
