d65a6515ef802893609710b06ff76e2bd778088e
Ingvar Jackal <rz703a@gmail.com>
2014-08-21 23:19:54 +0300

    fixed IndexOutOfBoundsException if no victims

--- src/main/scala/ru/org/codingteam/horta/plugins/pet/Pet.scala
+++ src/main/scala/ru/org/codingteam/horta/plugins/pet/Pet.scala
@@ -93,7 +93,7 @@ class Pet(roomId: String, location: ActorRef) extends Actor {
         coins = coins.mapValues(x => max(0, x - 1))
         sayToEveryone(location, s"$nickname" + pet.randomChoice(becomeDead) + ". Все теряют по 1PTC.")
       } else if (hunger == 16 || hunger <= 10 && pet.hunger > 7) { // hunger == 16 just adds more stochastic behaviour
-        if (pet.randomGen.nextInt(10) == 0) {
+        if (pet.randomGen.nextInt(10) == 0 && coins.keys.size > 0) {
           val victim = pet.randomChoice(coins.keys.toList)
           PtcUtils.updatePTC(victim, coins, -5)
           sayToEveryone(location, s"$nickname" + pet.randomChoice(aggressiveAttack) + victim + pet.randomChoice(loosePTC) + s". $victim теряет 5PTC.")
