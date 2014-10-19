d7e69c9e3eac58b492740e850e4d23329f3ba1a0
Friedrich von Never <friedrich@fornever.me>
2014-10-19 17:00:38 +0700

    Update application.conf location.

--- src/main/scala/ru/org/codingteam/horta/Application.scala
+++ src/main/scala/ru/org/codingteam/horta/Application.scala
@@ -1,11 +1,8 @@
 package ru.org.codingteam.horta
 
-import java.io.{PrintStream, File}
-
 import akka.actor.{ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.slf4j.Logging
-import org.slf4j.Logger
 import ru.org.codingteam.horta.configuration.Configuration
 import ru.org.codingteam.horta.core.Core
 
@@ -14,7 +11,7 @@ object Application extends App with Logging {
   override def main(args: Array[String]) {
     initializeConfiguration(args)
 
-    val system = ActorSystem("CodingteamSystem", ConfigFactory.parseFile(new File("application.conf")))
+    val system = ActorSystem("CodingteamSystem", ConfigFactory.parseResources("application.conf"))
     val core = system.actorOf(Props[Core], name = "core")
   }
 
