b06e1b544b38cc22ba9d445a12908e4984c62079
Friedrich von Never <friedrich@fornever.me>
2014-12-26 23:39:38 +0600

    Refactor ChangeNickCommand.

--- src/main/scala/ru/org/codingteam/horta/plugins/pet/commands/ChangeNickCommand.scala
+++ src/main/scala/ru/org/codingteam/horta/plugins/pet/commands/ChangeNickCommand.scala
@@ -1,42 +1,38 @@
 package ru.org.codingteam.horta.plugins.pet.commands
 
-import akka.actor.ActorRef
-import ru.org.codingteam.horta.plugins.pet.{PetData, PtcUtils}
-import ru.org.codingteam.horta.security.Credential
 import ru.org.codingteam.horta.localization.Localization._
+import ru.org.codingteam.horta.plugins.pet.PetData
+import ru.org.codingteam.horta.security.Credential
 
-class ChangeNickCommand extends AbstractCommand {
+class ChangeNickCommand extends PetCommand[String] {
+
+  override protected def parseArguments(arguments: Array[String])(implicit credential: Credential) = {
+    arguments match {
+      case Array("") => Right(localize("Empty string is not a valid pet nick."))
+      case Array(newNickname) => Left(newNickname.trim)
+      case _ => Right(localize("Try $pet help change-nick."))
+    }
+  }
+
+  override protected def price(nickname: String) =
+    (nickname.length + charactersPerPetcoin - 1) / charactersPerPetcoin
+
+  override protected def transactionName = "change pet nick"
+
+  override protected def onTransactionSuccess(pet: PetData, newNickname: String)(implicit credential: Credential) = {
+    val newPet = pet.copy(nickname = newNickname)
+    if (pet.alive) {
+      (newPet, localize("Now our pet's name is %s.").format(newNickname))
+    } else {
+      (newPet, localize("That's a surprise that our pet's name was %s when it was alive.").format(newNickname))
+    }
+  }
+
+  override protected def onTransactionFailure(pet: PetData, newNickname: String)(implicit credential: Credential) = {
+    val amount = price(newNickname)
+    (pet, localize("Insufficient PTC. You need %dPTC to set this nick.").format(amount))
+  }
+
   private val charactersPerPetcoin = 10
 
-  private def calcPriceOfNickname(nickName: String): Int = {
-    (nickName.length + charactersPerPetcoin - 1) / charactersPerPetcoin
-  }
-
-  private def changeNickname(pet: PetData, coins: ActorRef, credential: Credential, newNickname: String): (PetData, String) = {
-    implicit val c = credential
-    newNickname match {
-      case "" => (pet, localize("Empty string is not a valid pet nick."))
-      case _ =>
-        val changer = credential.name
-        val price = calcPriceOfNickname(newNickname)
-
-        if (PtcUtils.tryUpdatePTC(coins, changer, -price, "change pet nick") != 0) {
-          val newPet = pet.copy(nickname = newNickname)
-          if (pet.alive) {
-            (newPet, localize("Now our pet's name is %s.").format(newNickname))
-          } else {
-            (newPet, localize("That's a surprise that our pet's name was %s when it was alive.").format(newNickname))
-          }
-        } else {
-          (pet, localize("Insufficient PTC. You need %dPTC to set this nick.").format(price))
-        }
-    }
-  }
-
-  override def apply(pet: PetData, coins: ActorRef, credential: Credential, args: Array[String]): (PetData, String) = {
-    args match {
-      case Array(newNickname, _*) => changeNickname(pet, coins, credential, newNickname.trim)
-      case _ => (pet, localize("Try $pet help change-nick.")(credential))
-    }
-  }
 }
--- src/main/scala/ru/org/codingteam/horta/plugins/pet/commands/PetCommand.scala
+++ src/main/scala/ru/org/codingteam/horta/plugins/pet/commands/PetCommand.scala
@@ -6,21 +6,22 @@ import ru.org.codingteam.horta.security.Credential
 
 trait PetCommand[TParam] extends AbstractCommand {
 
-  protected def parseParams(params: Array[String]): Either[TParam, String]
+  protected def parseArguments(arguments: Array[String])(implicit credential: Credential): Either[TParam, String]
   protected def price(param: TParam): Int
   protected def transactionName: String
 
-  protected def onTransactionSuccess(pet: PetData, param: TParam): (PetData, String)
-  protected def onTransactionFailure(pet: PetData, param: TParam): (PetData, String)
+  protected def onTransactionSuccess(pet: PetData, param: TParam)(implicit credential: Credential): (PetData, String)
+  protected def onTransactionFailure(pet: PetData, param: TParam)(implicit credential: Credential): (PetData, String)
 
   override def apply(pet: PetData, coins: ActorRef, credential: Credential, args: Array[String]): (PetData, String) = {
-    parseParams(args) match {
-      case Left(param) => process(param, credential, coins, pet)
+    implicit val c = credential
+    parseArguments(args) match {
+      case Left(param) => process(param, coins, pet)
       case Right(error) => (pet, error)
     }
   }
 
-  private def process(param: TParam, credential: Credential, coins: ActorRef, pet: PetData) = {
+  private def process(param: TParam, coins: ActorRef, pet: PetData)(implicit credential: Credential) = {
     val amount = price(param)
     val user = credential.name
     if (executeTransaction(coins, user, amount) != 0) {
