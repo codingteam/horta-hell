e58d3e8fac932360fff3d9e768f9b7089d932ad6
Friedrich von Never <friedrich@fornever.me>
2015-02-16 21:41:44 +0600

    Refactor MailPlugin.

--- src/main/scala/ru/org/codingteam/horta/plugins/mail/MailMessage.scala
+++ src/main/scala/ru/org/codingteam/horta/plugins/mail/MailMessage.scala
@@ -1,3 +1,3 @@
 package ru.org.codingteam.horta.plugins.mail
 
-private case class MailMessage(id: Option[Int], room: String, senderNick: String, receiverNick: String, text: String)
+case class MailMessage(id: Option[Int], room: String, senderNick: String, receiverNick: String, text: String)
