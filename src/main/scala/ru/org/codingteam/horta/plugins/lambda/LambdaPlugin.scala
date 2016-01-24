350d596ca731eaf3fb3161e27945b71256300e61
rexim <reximkut@gmail.com>
2016-01-24 22:08:35 +0600

    Normal order input term (#364)

--- src/main/scala/ru/org/codingteam/horta/plugins/lambda/LambdaPlugin.scala
+++ src/main/scala/ru/org/codingteam/horta/plugins/lambda/LambdaPlugin.scala
@@ -4,6 +4,8 @@ import ru.org.codingteam.horta.plugins.{CommandDefinition, CommandProcessor, Bas
 import ru.org.codingteam.horta.protocol.Protocol
 import ru.org.codingteam.horta.security.{CommonAccess, Credential}
 
+import me.rexim.morganey.syntax.LambdaParser
+
 private object LambdaCommand
 
 class LambdaPlugin extends BasePlugin with CommandProcessor {
@@ -28,9 +30,19 @@ class LambdaPlugin extends BasePlugin with CommandProcessor {
     implicit val l = log
 
     (token, arguments) match {
-      case (LambdaCommand, Array(unparsedTerm, _*)) =>
-        Protocol.sendResponse(credential.location, credential, "khooy")
+      case (LambdaCommand, Array(unparsedTerm, _*)) => {
+        val term = LambdaParser.parse(LambdaParser.term, unparsedTerm)
+        if (term.successful) {
+          respond(term.get.normalOrder().toString)
+        } else {
+          respond(term.toString)
+        }
+      }
       case _ => // ignore
     }
+  }
+
+  private def respond(responseMessage: String)(implicit credential: Credential) = {
+    Protocol.sendResponse(credential.location, credential, responseMessage)
   }
 }
