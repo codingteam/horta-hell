76ff9380345107f9e3dec1513f43ebefe850ef9d
rexim <reximkut@gmail.com>
2016-01-28 23:35:29 +0600

    Load Morganey init script on Lambda plugin startup
    (#364)

--- src/main/scala/ru/org/codingteam/horta/plugins/lambda/LambdaPlugin.scala
+++ src/main/scala/ru/org/codingteam/horta/plugins/lambda/LambdaPlugin.scala
@@ -1,17 +1,34 @@
 package ru.org.codingteam.horta.plugins.lambda
 
+import java.io.InputStreamReader
+
 import me.rexim.morganey.ast.MorganeyBinding
 import ru.org.codingteam.horta.plugins.{CommandDefinition, CommandProcessor, BasePlugin}
 import ru.org.codingteam.horta.protocol.Protocol
 import ru.org.codingteam.horta.security.{CommonAccess, Credential}
 
 import me.rexim.morganey.syntax.LambdaParser
-import me.rexim.morganey.ast.LambdaTermHelpers._
-import me.rexim.morganey.ReplHelper
+import me.rexim.morganey.{MorganeyInterpreter, ReplHelper}
 
 private object LambdaCommand
 
 class LambdaPlugin extends BasePlugin with CommandProcessor {
+  private val initScriptFileName = "morganey/init.morganey"
+  private lazy val initScriptReader =
+    new InputStreamReader(ClassLoader.getSystemResourceAsStream(initScriptFileName))
+
+  private val globalContext: Seq[MorganeyBinding] =
+    MorganeyInterpreter
+      .interpretReader(initScriptReader, List())
+      .map (_.lastOption.map(_._2).getOrElse(List()))
+      .recover {
+        case t: Throwable => {
+          log.error(t, "Could not load morganey init script")
+          List()
+        }
+      }
+      .getOrElse(List())
+
   /**
     * Plugin name.
     * @return unique plugin name.
@@ -32,13 +49,11 @@ class LambdaPlugin extends BasePlugin with CommandProcessor {
     implicit val c = credential
     implicit val l = log
 
-    val context = Seq[MorganeyBinding](MorganeyBinding(lvar("I"), lfunc("x", lvar("x"))))
-
     (token, arguments) match {
       case (LambdaCommand, Array(unparsedTerm, _*)) => {
         val term = LambdaParser.parse(LambdaParser.term, unparsedTerm)
         if (term.successful) {
-          val result = term.get.addContext(context).normalOrder()
+          val result = term.get.addContext(globalContext).normalOrder()
           respond(ReplHelper.smartPrintTerm(result))
         } else {
           respond(term.toString)
