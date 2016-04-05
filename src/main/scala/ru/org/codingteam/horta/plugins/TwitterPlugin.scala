e71659656589032b880033a2bc9a249f42410650
Hagane <deemson@gmail.com>
2016-04-05 17:48:09 +0600

    implicits considered harmful and general improvements

--- src/main/scala/ru/org/codingteam/horta/plugins/TwitterPlugin.scala
+++ src/main/scala/ru/org/codingteam/horta/plugins/TwitterPlugin.scala
@@ -1,13 +1,13 @@
 package ru.org.codingteam.horta.plugins
 
-import akka.actor.ActorRef
+import akka.actor.{ActorLogging, ActorRef}
 import org.joda.time.DateTime
 import ru.org.codingteam.horta.localization.Localization
 import ru.org.codingteam.horta.messages.{Event, TwitterEvent}
 import ru.org.codingteam.horta.protocol.Protocol
 import ru.org.codingteam.horta.security.Credential
 
-class TwitterPlugin extends EventProcessor with RoomProcessor {
+class TwitterPlugin extends EventProcessor with RoomProcessor with ActorLogging {
 
   var rooms: Map[String,ActorRef] = Map()
 
@@ -16,12 +16,16 @@ class TwitterPlugin extends EventProcessor with RoomProcessor {
     case _ => false
   }
 
+  implicit val dispatcher = context.dispatcher
+
   /**
    * Process an event
    * @param event event to process
    */
-  override protected def processEvent(event: Event): Unit = {
+  override protected def processEvent(event: Event): Unit =
+  event match {
     case tweet: TwitterEvent => relayTweet(tweet)
+    case _ => log.warning("Unexpected event: {}", event)
   }
 
   def relayTweet(tweet: TwitterEvent): Unit = {
