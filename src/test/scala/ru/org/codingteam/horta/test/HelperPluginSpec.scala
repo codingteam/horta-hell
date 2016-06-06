65c8bf45647628220e6c183d69f39cd4a664ebe8
Friedrich von Never <friedrich@fornever.me>
2016-06-06 23:05:42 +0700

    HelperPluginSpec: remove the duplicated "should"

--- src/test/scala/ru/org/codingteam/horta/test/HelperPluginSpec.scala
+++ src/test/scala/ru/org/codingteam/horta/test/HelperPluginSpec.scala
@@ -22,26 +22,26 @@ class HelperPluginSpec extends TestKitSpec {
 
   "HelperPlugin" should {
     val plugin = system.actorOf(Props[HelperPlugin])
-    "should answer with some text when no arguments are given" in {
+    "answer with some text when no arguments are given" in {
       plugin ! ProcessCommand(credential, ManCommand, Array())
       val message = expectMsgType[SendResponse](timeout.duration)
       assert(!message.text.isEmpty)
       assert(!message.text.equals(errorMessage))
     }
-    "should answer with command usage text when given a command name as an argument" in {
+    "answer with command usage text when given a command name as an argument" in {
       plugin ! ProcessCommand(credential, ManCommand, Array("say"))
       val message = expectMsgType[SendResponse](timeout.duration)
       assert(!message.text.isEmpty)
       assert(!message.text.equals(errorMessage))
     }
-    "should answer with an error message when given more than one arguments" in {
+    "answer with an error message when given more than one arguments" in {
       plugin ! ProcessCommand(credential, ManCommand, Array("invalid", "arguments"))
       val message = expectMsgType[SendResponse](timeout.duration)
       assert(!message.text.isEmpty)
       assert(message.text.equals(errorMessage))
       assert(message.text.contains(credential.name))
     }
-    "should answer with an error message when given an unknown command name" in {
+    "answer with an error message when given an unknown command name" in {
       plugin ! ProcessCommand(credential, ManCommand, Array("notacommand"))
       val message = expectMsgType[SendResponse](timeout.duration)
       assert(!message.text.isEmpty)
